905e1abeb812e1011a5addebf35e328287fa8b09
Khim <94693293+LittleKhim@users.noreply.github.com>
2025-08-09 00:22:44 +0700

    Update mod.lua

--- mod.lua
+++ mod.lua
@@ -1,5 +1,10 @@
---
-{
-  id = 127826596841874,
-  name = "x2 Zombies"
+return {
+  {
+    id = 127826596841874,
+    name = "x2 Zombies"
+  },
+  {
+    id = 127826596841875,
+    name = "Night Vision"
+  }
 }
